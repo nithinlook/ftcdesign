d478fd06ada24d5aa8b0975e48dc2f9b153e14b3
Nithin <58347462+nithinnnn@users.noreply.github.com>
2025-04-30 17:26:50 -0700

    0.0.3 - edit somethings

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ftcdesign</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nithinlook/ftcdesign',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/nithinlook/ftcdesign',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'ftcdesign',
   },
 }
 
